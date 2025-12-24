d11d9761a4f4a935cee88ca00a8611af06618ac3
TsvetanValchinov <cecovalchinov@abv.bg>
2025-12-24 10:04:53 +0200

    fixed the app.py and requirements.txt

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({
-        "message": "Hello from DevOps Project!",
-        "status": "success"
+        "message": "DevOps Final Project: V2",
+        "status": "Running on Kubernetes",
+        "maintenance_by": "GitHub Actions"
     })
 
 @app.route('/health')
@@ -14,5 +15,4 @@ def health():
     return jsonify({"status": "healthy"})
 
 if __name__ == '__main__':
-    # Слушаме на всички интерфейси (0.0.0.0) на порт 5000
     app.run(host='0.0.0.0', port=5000)
