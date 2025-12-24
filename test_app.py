5b19434cb4a704e67fa34e831c049fc235ce6126
TsvetanValchinov <cecovalchinov@abv.bg>
2025-12-24 10:40:01 +0200

    updated tests

--- test_app.py
+++ test_app.py
@@ -8,11 +8,15 @@ def client():
         yield client
 
 def test_home(client):
-    """Test if the home page returns 200 OK and correct message"""
+    """Test if the home page returns 200 OK and correct V2 message"""
     rv = client.get('/')
     assert rv.status_code == 200
     json_data = rv.get_json()
-    assert json_data['message'] == "Hello from DevOps Project!"
+
+    assert json_data['message'] == "DevOps Final Project: V2"
+    assert json_data['status'] == "Running on Kubernetes"
+    assert json_data['maintenance_by'] == "GitHub Actions"
+
 
 def test_health(client):
     """Test the health check endpoint"""
